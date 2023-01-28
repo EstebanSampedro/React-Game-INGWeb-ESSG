6433f0d421c9735e55e3360f54bcf328da947267
Esteban Sampedro <sebasgar11@gmail.com>
2023-01-27 21:18:04 -0500

    Connections & packages updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
 import './App.css';
-import Routes from './routes'
+import React from 'react';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+
+import Home from '../src/pages/Home'
+import Game from '../src/pages/Game'
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/game" element={<Game/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
