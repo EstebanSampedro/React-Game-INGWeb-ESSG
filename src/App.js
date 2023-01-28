89622867fc0ea9b70b3df9e6ee8b46412f0cc9e8
Esteban Sampedro <sebasgar11@gmail.com>
2023-01-27 21:30:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Game/>}/>
         <Route path="/game" element={<Game/>}/>
       </Routes>
     </BrowserRouter>
